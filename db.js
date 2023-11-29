dc7aca64af39f6935164682dab55b6f627648c0e
Rachit <mailracit@gmail.com>
2023-11-29 15:32:26 +0530

    updated app listening function

--- db.js
+++ db.js
@@ -7,9 +7,7 @@ async function start() {
     module.exports = client
     console.log("db conntected")
     const app = require('./server')
-    app.listen(process.env.PORT, "192.168.1.3" || "localhost" ,() => {
-        console.log(`Listening to request on http://localhost:${process.env.PORT}`)
-    })
+    app.listen(process.env.PORT)
 }
 
 start()
