029a9c59f6bb5797cdfb47bc6a01a29140c95115
prashant <prashant.sharma@makear.ai>
2024-03-08 13:56:41 +0530

    Added Navlinks Overlay

--- public/js/script-new.js
+++ public/js/script-new.js
@@ -1,39 +1,76 @@
 const menuBtn = document.querySelector(".menuBtn");
+const menuOverlay = document.querySelector(".overlay");
+const overlayLinks = document.querySelectorAll(".overlay a");
 
 let openMenu = false;
+let activeMenu = 0;
 
 menuBtn.addEventListener("click", function () {
-  //   menuBtn.children[0].classList.add("move-up-animations");
-  //   setTimeout(() => {
-  //     menuBtn.children[0].classList.remove("move-up-animations");
-  //   }, 400);
-
   openMenu = !openMenu;
-  if (openMenu) {
-    // console.log(this.children[1].children[0]);
-    // console.log("Open");
-    // this.children[0].textContent = "CLOSE";
-    // console.log(this.children[0].textContent);
-    this.children[2].children[0].classList.add("openMenu");
-    this.children[0].classList.toggle("move-up");
-    this.children[1].classList.toggle("move-down");
-  } else if (!openMenu) {
-    // console.log("Closed");
 
-    if (this.children[2].children[0].classList.contains("openMenu")) {
-      this.children[2].children[0].classList.remove("openMenu");
-      this.children[0].classList.toggle("move-up");
-      this.children[1].classList.toggle("move-down");
-      //   this.children[0].textContent = "MENU";
-      //   console.log(this.children[0].textContent);
+  if (openMenu) {
+    menuBtn.children[2].children[0].classList.add("openMenu");
+    menuBtn.children[0].classList.toggle("move-up");
+    menuBtn.children[1].classList.toggle("move-down");
+
+    menuOverlay.style.opacity = 1;
+    menuOverlay.style.visibility = "visible";
+    menuOverlay.style.transform = "translateY(0%)  rotate(0deg)";
+    menuOverlay.style.transition =
+      "opacity 1s ease, visibility .5s , transform 1s";
+  } else if (!openMenu) {
+    if (menuBtn.children[2].children[0].classList.contains("openMenu")) {
+      menuBtn.children[2].children[0].classList.remove("openMenu");
+      menuBtn.children[0].classList.toggle("move-up");
+      menuBtn.children[1].classList.toggle("move-down");
+
+      menuOverlay.style.opacity = 0;
+      menuOverlay.style.visibility = "hidden";
+      menuOverlay.style.transform = "translateY(100%) rotate(8deg)";
+      menuOverlay.style.transition =
+        "opacity 0.5s ease, visibility 0.5s, transform 0.5s";
     }
   }
 });
 
-// function animateBtn(button) {
-//   console.log(button);
-//   button.children[0].classList.add("move-up-animations");
-//   setTimeout(() => {
-//     button.children[0].classList.remove("move-up-animations");
-//   }, 800);
-// }
+overlayLinks.forEach(function (link) {
+  link.addEventListener("mouseenter", function () {
+    this.children[0].classList.add("moveUpAnimation");
+
+    setTimeout(() => {
+      this.children[0].classList.remove("moveUpAnimation");
+    }, 400);
+  });
+
+  link.addEventListener("mouseleave", function () {
+    this.children[0].classList.add("reverseMoveUpAnimation");
+
+    setTimeout(() => {
+      this.children[0].classList.remove("reverseMoveUpAnimation");
+    }, 400);
+  });
+});
+
+menuOverlay.addEventListener("click", (e) => {
+  const menuItem = e.target.closest(".menuItem");
+  const activeLink = menuOverlay.querySelector(".menuItem.activeMenu");
+
+  if (menuItem) {
+    e.preventDefault();
+    if (activeLink !== menuItem) {
+      if (activeLink) {
+        activeLink.classList.remove("activeMenu");
+
+        const activeImg = activeLink.querySelector("img");
+
+        activeImg.classList.remove("activeDot");
+        activeImg.setAttribute("src", `assets/images-new/arrow-dark.svg`);
+      }
+      menuItem.classList.add("activeMenu");
+      const img = menuItem.querySelector("img");
+
+      img.classList.add("activeDot");
+      img.setAttribute("src", `assets/images-new/single-dot-dark.svg`);
+    }
+  }
+});
