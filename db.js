609caab5810d1643ff59324a9e824e9fbdaddcdc
rachit <mailracit@gmail.com>
2024-03-11 11:30:46 +0530

    smooth menu items transitions

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ async function start() {
   module.exports = client;
   console.log("db conntected");
   const app = require("./server");
-  app.listen(process.env.PORT, "192.168.1.22");
+  app.listen(process.env.PORT, '192.168.1.14');
 }
 
 start();
