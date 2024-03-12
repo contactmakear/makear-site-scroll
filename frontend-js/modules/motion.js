a8a2e0482849c3bae02ccf831f2378bd02b1e023
rachit <mailracit@gmail.com>
2024-03-12 12:34:11 +0530

    navbar animation and bootstrap cdn

--- frontend-js/modules/motion.js
+++ frontend-js/modules/motion.js
@@ -1,13 +1,17 @@
 import SplitType from 'split-type'
 import { animate, stagger } from 'motion'
 
-// Nav bar animation
-const navIntroPara = new SplitType('#nav-intro-para', { types: 'lines' })
-const siteLogo = document.querySelector('.site-logo')
-const menuBtn = document.querySelector('.btns')
-const navElements = [siteLogo, ... navIntroPara.lines, menuBtn]
+export default function motionAnimation() {
 
-animate(navElements, { y: [24, 0], opacity: [0, 1]}, {duration: 0.8, delay: stagger(0.05)})
+  // Nav bar animation
+  const navIntroPara = new SplitType('#nav-intro-para', { types: 'lines' })
+  const siteLogo = document.querySelector('.site-logo')
+  const menuBtn = document.querySelector('.btns')
+  const navElements = [siteLogo, ... navIntroPara.lines, menuBtn]
 
-// Hero section animation
-//code ...
+  animate(navElements, { y: [24, 0], opacity: [0, 1]}, {duration: 0.8, delay: stagger(0.05)})
+
+  // Hero section animation
+  
+  
+}
